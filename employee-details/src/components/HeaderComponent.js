4017cd775a4bfd06d9de58e82aa4361fbcfd76c2
Abhinav Dubey <dubey,abhinav76@gmail.com>
2020-09-30 21:48:10 +0530

    headerComponent added

--- employee-details/src/components/HeaderComponent.js
+++ employee-details/src/components/HeaderComponent.js
@@ -1,1 +1,14 @@
 
+import React from "react";
+
+export default function HeaderComponentOne() {
+    return <h1>TechnoFunnel.in One</h1>
+}
+
+export function HeaderComponentTwo() {
+    return <h1>TechnoFunnel.in Two</h1>
+}
+
+export function HeaderOtherComponent() {
+    return <h1>This is Other Header Component</h1>
+}
